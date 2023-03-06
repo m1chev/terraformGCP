406672ef7391869d5835b5db0034efee9d098e86
michev <veselinov.michev@gmail.com>
2023-03-06 10:43:01 +0200

    Creating two instances in order to create cluster

--- main.tf
+++ main.tf
@@ -5,8 +5,29 @@ provider "google" {
   project     = var.project_id
   credentials = var.credential_file
 }
-resource "google_compute_instance" "default" {
-  name         = var.instance_name
+
+resource "google_compute_instance" "k8s-1" {
+  name         = var.instance_name_1
+  machine_type = var.instance_type
+  zone         = var.instance_zone
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = var.distro_image
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "k8s-2" {
+  name         = var.instance_name_2
   machine_type = var.instance_type
   zone         = var.instance_zone
   tags         = ["ssh"]
--- variables.tf
+++ variables.tf
@@ -10,9 +10,14 @@ variable "credential_file" {
   default     = "/home/michev/.ssh/vrgn-playground-6bd8dde9f143.json"
 }
 
-variable "instance_name" {
+variable "instance_name_1" {
   type    = string
-  default = "kubernetes-practice"
+  default = "k8s-practice-1"
+}
+
+variable "instance_name_2" {
+  type    = string
+  default = "k8s-practice-2"
 }
 
 variable "instance_type" {
