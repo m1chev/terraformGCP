b445f22c36d73f74f44a9321c5974ab54511f8af
michev <veselinov.michev@gmail.com>
2023-03-16 22:01:51 +0200

    Add Ansible and firewall config
    Ansible is needed for installing k3s, helm, nginx.
    Firewall configuration is for opening the ports to outside.

--- main.tf
+++ main.tf
@@ -6,11 +6,24 @@ provider "google" {
   credentials = var.credential_file
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "k8s"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["30000-32767"]
+  }
+
+  target_tags= ["k8s"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
 resource "google_compute_instance" "k8s-1" {
   name         = var.instance_name_1
   machine_type = var.instance_type
   zone         = var.instance_zone
-  tags         = ["ssh"]
+  tags         = ["ssh","k8s"]
 
   boot_disk {
     initialize_params {
@@ -21,6 +34,7 @@ resource "google_compute_instance" "k8s-1" {
   network_interface {
     # A default network is created for all GCP projects
     network = "default"
+    network_ip = "10.164.0.29"
     access_config {
     }
   }
